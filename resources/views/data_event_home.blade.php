2c67a0fefee9d091e6d48b35b8c3ab162d9a07e5
thainbahuy <thainbahuy@gmail.com>
2019-08-26 22:35:17 +0700

    done css hover image home

--- resources/views/data_event_home.blade.php
+++ resources/views/data_event_home.blade.php
@@ -3,18 +3,21 @@
 {{--        <div class="item-media background-parallax"--}}
 {{--             data-background="{{asset($item->image_cover)}}">--}}
 {{--        </div>--}}
-        <img class="item-media background-parallax img-thumbnail"  src="{{$item->image_cover}}" alt="{{$item->name}}">
+        @php
+            $url= [
+                'event' => str_slug($item->name)."-post-".$item->id,
+            ];
+        @endphp
+        <a href="{{route('web.event-detail',$url)}}">
+            <img class="item-media background-parallax img-thumbnail"  src="{{$item->image_cover}}" alt="{{$item->name}}">
+        </a>
         <figcaption>
             <div class="item-caption-inner">
                 <p class="text-links">
                     {{$item->author}}
                 </p>
                 <h5 class="bold uppercase">{{$item->name}}</h5>
-                @php
-                    $url= [
-                        'event' => str_slug($item->name)."-post-".$item->id,
-                    ];
-                @endphp
+
                 <a class="button style-3 border-2" href="{{route('web.event-detail',$url)}}">@lang('Message.Watch now')</a>
             </div>
         </figcaption>
