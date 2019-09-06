4a6f9bff6eda1c2c14910e02116e9a0e45326d5e
ba huy thai nguyen <thainguyenbahuy@gmail.com>
2019-09-07 00:53:36 +0700

    fix upload js

--- public/admin/js/upload.js
+++ public/admin/js/upload.js
@@ -1,21 +1,18 @@
 $("#image_choose_btn").change(function () {
-
     if (this.files && this.files[0] && this.files[0].size < 3000000) {
         var reader = new FileReader();
-        reader.onload = function(e) {
+        reader.onload = function (e) {
             //upload
-            uploadImageToServer(e.target.result,2)
+            uploadImageToServer(e.target.result, 2)
         }
         reader.readAsDataURL(this.files[0]);
-
     } else {
         alert('file to large or not choose yet !');
         $("#image_choose_btn").val('');
     }
-
 });
 
-function uploadImageToServer(image,option) {
+function uploadImageToServer(image, option) {
     $('#loader').show();
     $.ajax(
         {
@@ -27,7 +24,7 @@ function uploadImageToServer(image,option) {
             },
         }).done(function (response) {
         //preview
-        $('.imgKLIK5').attr('src',image);
+        $('.imgKLIK5').attr('src', image);
         //set input
         $('#image_cover').val(response.link);
         $('#image_cover_key').val(response.deletehash);
